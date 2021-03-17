e2ee74e8a2ea8a4360bc83b8b2b05f514111c4fe
Aydrian Howard <aydrian@gmail.com>
2021-03-17 11:58:32 -0400

    Added mocked NavBar

---
+++ components/NavBar/index.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import {
+  Avatar,
+  Box,
+  Flex,
+  HStack,
+  Text,
+  useColorModeValue
+} from "@chakra-ui/react";
+
+export default function NavBar() {
+  return (
+    <Box bg={useColorModeValue("gray.100", "gray.900")} px="4">
+      <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+        <Box>
+          <Link href="/">
+            <Text fontWeight="medium">Worst Buy</Text>
+          </Link>
+        </Box>
+        <HStack>
+          <Text>Welcome, Aydrian</Text>
+          <Avatar
+            size={"sm"}
+            src={
+              "https://pbs.twimg.com/profile_images/1250423206931861505/3-52t24v_400x400.jpg"
+            }
+          />
+        </HStack>
+      </Flex>
+    </Box>
+  );
+}
--- layouts/main.js
+++ layouts/main.js
@@ -1,13 +1,17 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 import Header from "@components/Header";
+import NavBar from "@components/NavBar";
 import Footer from "@components/Footer";
 
 export default function MainLayout(props) {
   return (
     <Flex direction="column" height="100vh">
       <Header />
-      <main>{props.children}</main>
+      <NavBar />
+      <Box as="main" flex="1" p="4">
+        {props.children}
+      </Box>
       <Footer />
     </Flex>
   );
